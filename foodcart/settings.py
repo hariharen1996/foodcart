3adc77aea93c55a140d82aa76c252f224d6e0ef6
hariharen96 <hariharen1996@gmail.com>
2025-04-21 15:51:53 +0530

    feat: Add registration api with token creation Created UserSerializer to validate and serialize user data with username,email,password,phone,address Implemented a CreateUserView to handle user registration using class based views and added permissions Added users app in root urls config and Added register endpoint in users app urls.py Updated installed apps with rest_framework.authtoken Added TokenAuthentication as the default authentication class in REST_FRAMEWORK settings.py Tested the endpoints POST: api/users/register in postman

--- foodcart/settings.py
+++ foodcart/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
+    'rest_framework.authtoken',
     'users'
 ]
 
@@ -125,3 +126,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
+
--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import AllowAny
+from . import serializers
 
 # Create your views here.
+class CreateUserView(CreateAPIView):
+    serializer_class = serializers.UserSerializer
+    permission_classes = [AllowAny]
