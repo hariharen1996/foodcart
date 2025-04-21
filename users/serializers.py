b004270301d5d7eb3c9be3bc33f90e30604e333b
hariharen96 <hariharen1996@gmail.com>
2025-04-21 19:01:43 +0530

    feat: add login view with token authentication Implemented LoginView using custom AuthTokenSerializer Refactored UserSerializer by fixing the password hashing using set_pasword Added urls routing for login using the endpoint login/ Tested the endpoint POST: api/users/login using postman

--- users/serializers.py
+++ users/serializers.py
@@ -11,9 +11,32 @@ class UserSerializer(serializers.ModelSerializer):
         extra_kwargs = { 'password': { 'write_only': True } }
     
     def create(self,validated_data):
-        user = User.objects.create(username=validated_data['username'],email=validated_data['email'],
-                                password=validated_data['password'],phone=validated_data.get('phone',''),
-                                address=validated_data.get('address',''))
-        
+        user = User.objects.create(username=validated_data['username'],email=validated_data['email'],phone=validated_data.get('phone',''),address=validated_data.get('address',''))
+        user.set_password(validated_data['password'])
+        user.save()
         Token.objects.create(user=user)
         return user
+
+class AuthTokenSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField()
+
+    def validate(self, attrs):
+        email = attrs.get('email').lower()
+        password = attrs.get('password')
+
+        if email and password:
+            user = User.objects.filter(email=email).first()
+            if user:
+                if not user.check_password(password):
+                    message = "credentials are invalid, unable to login"
+                    raise serializers.ValidationError(message,code='authorization')
+            else:
+                message = 'user with this email does not exists'
+                raise serializers.ValidationError(message,code='authorization')
+            
+            attrs['user'] = user 
+            return attrs
+        else:
+            message = 'email and password is mandetory'
+            raise serializers.ValidationError(message,code='authorization')
--- users/views.py
+++ users/views.py
@@ -2,8 +2,21 @@ from django.shortcuts import render
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import AllowAny
 from . import serializers
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
 
 # Create your views here.
 class CreateUserView(CreateAPIView):
     serializer_class = serializers.UserSerializer
     permission_classes = [AllowAny]
+
+class LoginView(ObtainAuthToken):
+    serializer_class = serializers.AuthTokenSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializers = self.serializer_class(data=request.data,context={'request':request})
+        serializers.is_valid(raise_exception=True)
+        user = serializers.validated_data['user']
+        token,created = Token.objects.get_or_create(user=user)
+        return Response({'token':token.key,'user_id':user.pk,'email':user.email})
