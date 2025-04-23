d7e364a42d46c841dabe9721447f27f42b61d8ee
hariharen96 <hariharen1996@gmail.com>
2025-04-23 17:06:49 +0530

    feat: Add CRUD for Category model Created CategorySerializer with id, name, description, created_at, updated_at fields Implemented ListCreateAPIView for creating and retrieveing categories Implemented RetrieveUpdateDestroyAPIView for managing individual categories Included the urls for restaurant app in root urls.py Added endpoints for categories in restaurant app urls.py for categories Tested the endpoints POST | GET: api/restaurant/categories, GET: api/restaurant/categories/{id}/ using postman Tested the endpoints PUT: api/restaurant/categories/{id}/, DELETE: api/restaurant/categories/{id}/ using postman

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.generics import ListCreateAPIView,RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import IsAuthenticated
+from .serializer import CategorySerializer
+from .models import Category
 
 # Create your views here.
+class CategoryListCreateAPIView(ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [IsAuthenticated]    
+
+class CategoryRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [IsAuthenticated]
